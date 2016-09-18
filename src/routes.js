7e593d9cfb2e576122fff64083dc1d2891e3a4f6
moravemj <mmoravec09@gmail.com>
2016-09-18 09:27:22 -0700

    redux form example done

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostShow from './components/posts_show';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostShow} />
+  </Route>
+);
+
+// google.com/  => renders App
